9f51854b1fa25ca33f545a92597ad0a6194e7585
CHELSEA TH FANs <kidsada.tan@student.mbu.ac.th>
2026-05-21 08:29:38 +0700

    Add club logos data file

---
+++ data/club-logos.ts
@@ -0,0 +1,10 @@
+export const clubLogos = {
+  chelsea: "https://i.postimg.cc/yNs5vXxg/CHELSEA.png",
+  tottenham: "https://i.postimg.cc/SQD8GDyz/TOTTENHAM-HOTSPUR.png",
+  acMilan: "https://i.postimg.cc/pXJPMhQv/AC-Milan.jpg",
+  allStarsWomen: "https://i.postimg.cc/SNfmb2Lp/All-Stars-Women-b.jpg",
+  aucklandFC: "https://i.postimg.cc/vHtGwg7G/Auckland-FC.png",
+  johor: "https://i.postimg.cc/Ghxbn8Pr/Johor-Darul-Ta-zim-F-C.jpg",
+  westernSydney: "https://i.postimg.cc/65fBx2Vt/Western-Sydney-Wanderers-FC.jpg",
+  juventus: "https://i.postimg.cc/y8h4kJWg/juventus.jpg"
+};
